05cd94781da329654367b0b359df6312a3db5ce1
rolands.kalituha <Kalituha21@gmail.com>
2022-05-04 01:10:23 +0300

    Sections access under roles

--- controllers/ExamplesController.php
+++ controllers/ExamplesController.php
@@ -2,10 +2,36 @@
 
 namespace app\controllers;
 
+use app\models\User;
+use Yii;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 
 class ExamplesController extends Controller
 {
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'only' => ['student'],
+                'rules' => [
+                    [
+                        'actions' => ['student'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                        'matchCallback' => function () {
+                            return in_array(
+                                Yii::$app->user->identity->role,
+                                [User::ROLE_ADMIN, User::ROLE_INSTRUCTOR]
+                            );
+                        },
+                    ],
+                ],
+            ],
+        ];
+    }
+
     public function actionIndex()
     {
         return $this->render(
